5e1ef5e355d36c240bb5f227d7491b70b71ce80c
jay0425 <heedev94@gmail.com>
2024-04-07 00:10:45 +0900

    nav bar css 수정

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -27,19 +27,19 @@ const AppLayout = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <a class="nav-link text-white" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link text-white" href="#">
                   Movies
                 </a>
               </li>
             </ul>
             <form class="d-flex" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">
+              <button class="btn btn-outline-danger" type="submit">
                 Search
               </button>
             </form>
