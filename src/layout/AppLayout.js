e2f9a3e1cecd4defa8a1cd24096460000f352b8a
jay0425 <heedev94@gmail.com>
2024-04-07 22:10:09 +0900

    input 검색창 css 수정

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -34,7 +34,12 @@ const AppLayout = () => {
               </li>
             </ul>
             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <input
+                class="form-control me-2 outline-success bg-success text-white"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
               <button class="btn btn-outline-success" type="submit">
                 Search
               </button>
