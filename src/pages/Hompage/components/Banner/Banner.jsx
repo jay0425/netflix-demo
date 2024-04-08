ba1fab54380bb20e7a3bcec70225ecb684c3978c
jay0425 <heedev94@gmail.com>
2024-04-08 12:12:01 +0900

    배너이미지 화질 개선

--- src/pages/Hompage/components/Banner/Banner.jsx
+++ src/pages/Hompage/components/Banner/Banner.jsx
@@ -14,10 +14,16 @@ const Banner = () => {
     return <Alert variant="danger">{error.message}</Alert>;
   }
   return (
+    // <div
+    //   style={{
+    //     backgroundImage:
+    //       'url(' + `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].backdrop_path}` + ')',
+    //   }}
+    //   className="banner"
+    // >
     <div
       style={{
-        backgroundImage:
-          'url(' + `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` + ')',
+        backgroundImage: 'url(' + `https://image.tmdb.org/t/p/original${data?.results[0].backdrop_path}` + ')',
       }}
       className="banner"
     >
