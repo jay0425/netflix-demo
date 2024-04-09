c9db7ca48c6f2275f02c057d202af0456290cde8
jay0425 <heedev94@gmail.com>
2024-04-09 21:56:37 +0900

    common폴더, constatn폴더 생성

--- src/pages/Hompage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Hompage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,31 +1,9 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { Alert } from 'react-bootstrap';
-import MovieCard from '../MovieCard/MovieCard';
 import Loading from '../Loading/Loading';
-import './PopularMovieSlide.style.css';
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 10,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 7,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+import MovieSlider from '../../../../common/MovieSlider/MovieSlider';
+import { responsive } from '../../../../constants/responsive';
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -34,23 +12,12 @@ const PopularMovieSlide = () => {
     return <Loading />;
   }
   if (isError) {
-    return <Alert varient="danger">{error.message}</Alert>;
+    return <Alert variant="danger">{error.message}</Alert>;
   }
 
   return (
     <div>
-      <h3 className="about-movie">Popular Movies</h3>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
-        responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
+      <MovieSlider title="Popular Movies" movies={data.results} responsive={responsive} />
     </div>
   );
 };
