995614c09840f781ba650425500460d5d01401f9
jay0425 <heedev94@gmail.com>
2024-04-14 18:44:17 +0900

    detail 페이지 영화 기본 정보 입력

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Alert, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useDetailMovies } from '../../hooks/useDeatilMovies';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImdb } from '@fortawesome/free-brands-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 const MovieDetailPage = () => {
   let { id } = useParams();
@@ -27,7 +30,24 @@ const MovieDetailPage = () => {
             />
           </Col>
           <Col>
-            <h1 className="py-2">{data?.title}</h1>
+            <h1>{data?.title}</h1>
+            <div className="d-flex align-items-center">
+              <h3 className="me-4">
+                <FontAwesomeIcon icon={faImdb} className="me-2" />
+                {data?.vote_average}
+              </h3>
+              <h3 className="me-4">
+                <FontAwesomeIcon icon={faUsers} className="me-2 text-warning" />
+                {data?.popularity}
+              </h3>
+              <div>
+                {data.adult ? (
+                  <div className="bg-danger rounded-circle adult-style">18</div>
+                ) : (
+                  <div className="bg-warning rounded-circle text-black adult-style">ALL</div>
+                )}
+              </div>
+            </div>
             <div>영화 정보</div>
           </Col>
         </Row>
